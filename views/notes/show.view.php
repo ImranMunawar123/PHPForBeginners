a15725736b3a3c81a33f3543396481959c743010
imi.munawar <imi.munawar@gmail.com>
2023-09-29 12:12:59 +0500

    Build a better router

--- views/notes/show.view.php
+++ views/notes/show.view.php
@@ -6,6 +6,12 @@
         <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
             <a href="/notes" style="color: dodgerblue; text-decoration: underline">Go Back</a>
             <p><?= htmlspecialchars($note['body']) ?></p>
+
+            <form class="mt-6" method="POST">
+                <input type="hidden" name="_method" value="DELETE">
+                <input type="hidden" name="id" value="<?= $note['id'] ?>">
+                <button class="text-sm text-red-500">Delete</button>
+            </form>
         </div>
     </main>
 
